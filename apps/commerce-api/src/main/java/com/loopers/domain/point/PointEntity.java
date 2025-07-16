bd6e31f20b2ece8246758b70fc957d78c1183f7c
jinunu <mybark7@gmail.com>
2025-07-16 21:56:59 +0900

    test : 포인트 충전 단위 테스트

--- apps/commerce-api/src/main/java/com/loopers/domain/point/PointEntity.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/point/PointEntity.java
@@ -1,6 +1,8 @@
 package com.loopers.domain.point;
 
 import com.loopers.domain.BaseEntity;
+import com.loopers.support.error.CoreException;
+import com.loopers.support.error.ErrorType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,4 +18,10 @@ public class PointEntity extends BaseEntity {
 
     private String userId;
     private Long amount;
+
+    public void chargeAmount(Long amount) {
+        if (amount <= 0) {
+            throw new CoreException(ErrorType.BAD_REQUEST, "0이하의 포인트는 충전 할 수 없습니다.");
+        }
+    }
 }
